89beb273f8838db7706ae93f80fd4f031a1f17e8
Bhoomika Mewada <58257045+Bhoomika2820@users.noreply.github.com>
2020-04-04 10:48:14 +0530

    Add files via upload

---
+++ SJF.py
@@ -0,0 +1,56 @@
+#sjf
+list1 = []
+start = []
+que = []
+n = int(input("NO of processes : "))
+print("Process\tBurst time\tArrival time")
+for i in range(n):
+    input1 = list(map(int, input().split()))
+    list1.append(input1)
+p=list(list1)
+p.sort(key=lambda x: x[1])
+for i in range(n):
+    start.append(p[i][0])
+
+c = 0
+while len(start) != 0:
+    for j in start:
+        print(j,start)
+        if list1[j-1][2] <= c:
+            list1[j-1].append(c - list1[j-1][2])
+            list1[j-1].append(c + list1[j-1][1])
+            c = c + list1[j-1][1]
+            start.remove(j)
+            que.append(j)
+            break
+
+# c = 0
+# seq = []
+# for i in range(n):
+#     min1 = btl.index(min(btl))
+#     btl[min1] = 999999
+#     if (list1[min1][2] <= c):
+#         seq.append(list1[min1][0])
+#         listj = list(list1[min1])
+#         listj.append(c)
+#         listj.append(c + listj[1])
+#         c = c + listj[1]
+#         t = tuple(listj)
+#         list1[min1] = t
+
+print("Process Sequence : ", end='')
+for each in que:
+    print(each, end=' ')
+print("\nProcess Burst time Arrival time Waiting time Completion time")
+for each in que:
+    print(list1[each-1][0],list1[each-1][1],list1[each-1][2],list1[each-1][3],list1[each-1][4],sep="\t\t\t")
+awt = 0
+print("\nAverage waiting time : ", end="")
+for i in range(n):
+    awt += list1[i][3]
+print("{:.2f}".format(awt / n))
+ct = 0
+print("Average Turnaround time : ", end="")
+for i in range(n):
+    ct += list1[i][4] - list1[i][2]
+print("{:.2f}".format(ct / n))
